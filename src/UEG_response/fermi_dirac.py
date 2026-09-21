64f34a61c13d6118aa9a909330856ffa344cb0f0
Pontus Svensson <psvensson96@gmail.com>
2026-09-21 13:50:14 +0200

    Added license

--- src/UEG_response/fermi_dirac.py
+++ src/UEG_response/fermi_dirac.py
@@ -1,3 +1,19 @@
+# This file is part of the UEG-response code for computations of linear and nonlinear response functions.
+# Copyright (C) 2026  Pontus Svensson
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 import numpy as np
 from scipy.integrate import quad
 from scipy.optimize import root_scalar
